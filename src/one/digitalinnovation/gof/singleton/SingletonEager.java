0513f3f2060d875338b2b175e07d45c2c030f5c8
Daniela Flemming <daniflemming@gmail.com>
2022-07-19 17:12:13 -0300

    first commit

---
+++ src/one/digitalinnovation/gof/singleton/SingletonEager.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "apressado".
+ * 
+ * @author Flemming27
+ */
+
+public class SingletonEager {
+
+	private static SingletonEager instancia = new SingletonEager();
+	
+	private SingletonEager() {
+		super();
+	}
+	
+	public static SingletonEager getInstancia() {
+		return instancia;
+	}
+}
